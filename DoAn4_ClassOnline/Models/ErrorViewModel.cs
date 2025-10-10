d2ca55a26c74ee9916dbaba505db5c38b4ca82d4
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-10-10 13:20:37 +0700

    Add project files.

---
+++ DoAn4_ClassOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAn4_ClassOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
