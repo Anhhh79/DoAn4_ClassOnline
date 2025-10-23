57f8bd9aa11733010d4b696227884cbf62afd1da
QuocBao <quocbao272004@gmail.com>
2025-10-23 17:34:55 +0700

    comiiitttt

---
+++ DoAn4_ClassOnline/wwwroot/assets/js/student/KhoaHoc.js
@@ -0,0 +1,20 @@
+﻿function loadNoiDung(url) {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById("noiDungContainer").innerHTML = html;
+        })
+        .catch(err => console.error("Lỗi tải nội dung:", err));
+}
+
+// Khi bấm Thông báo
+document.getElementById("btnThongBao").addEventListener("click", function (e) {
+    e.preventDefault();
+    loadNoiDung('/Student/Notification/Index');
+});
+
+// Khi bấm Tài liệu
+document.getElementById("btnTaiLieu").addEventListener("click", function (e) {
+    e.preventDefault();
+    loadNoiDung('/Student/Document/Index');
+});
