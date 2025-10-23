b5685a0080169d21eff08fe939af8f11624bcb86
QuocBao <quocbao272004@gmail.com>
2025-10-23 15:16:11 +0700

    commit

---
+++ DoAn4_ClassOnline/Areas/Student/Controllers/KhoaHoc_1Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAn4_ClassOnline.Areas.Student.Controllers
+{
+	[Area("Student")]
+	public class KhoaHoc_1Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
