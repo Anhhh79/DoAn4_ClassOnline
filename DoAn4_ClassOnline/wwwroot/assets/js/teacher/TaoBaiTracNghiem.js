8839345be9893e083b9e09b89b36b0da3a8a4770
QuocBao <quocbao272004@gmail.com>
2025-11-10 20:51:09 +0700

    fix js

--- DoAn4_ClassOnline/wwwroot/assets/js/teacher/TaoBaiTracNghiem.js
+++ DoAn4_ClassOnline/wwwroot/assets/js/teacher/TaoBaiTracNghiem.js
@@ -59,16 +59,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const checkAll = document.getElementById('checkAllStudentsStatic');
     const checkboxes = document.querySelectorAll('.student-checkbox');
 
-    // Tick tất cả / bỏ chọn tất cả
-    checkAll.addEventListener('change', function () {
-        checkboxes.forEach(cb => cb.checked = checkAll.checked);
-    });
-
-    // Nếu bỏ chọn 1 ô => bỏ tick "chọn tất cả"
-    checkboxes.forEach(cb => {
-        cb.addEventListener('change', function () {
-            if (!cb.checked) checkAll.checked = false;
-            else if (Array.from(checkboxes).every(x => x.checked)) checkAll.checked = true;
+    if (checkAll) {
+        // Tick tất cả / bỏ chọn tất cả
+        checkAll.addEventListener('change', function () {
+            checkboxes.forEach(cb => cb.checked = checkAll.checked);
         });
-    });
+
+        // Nếu bỏ chọn 1 ô => bỏ tick "chọn tất cả"
+        checkboxes.forEach(cb => {
+            cb.addEventListener('change', function () {
+                if (!cb.checked) {
+                    checkAll.checked = false;
+                } else if (Array.from(checkboxes).every(x => x.checked)) {
+                    checkAll.checked = true;
+                }
+            });
+        });
+    } else {
+       
+    }
 });
