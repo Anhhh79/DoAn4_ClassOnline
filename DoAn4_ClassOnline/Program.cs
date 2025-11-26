f2bcac843df1e794cb344fcaf161ed9000029e69
QuocBao <quocbao272004@gmail.com>
2025-11-26 17:43:55 +0700

    update

--- DoAn4_ClassOnline/Program.cs
+++ DoAn4_ClassOnline/Program.cs
@@ -23,7 +23,7 @@ app.UseAuthorization();
 // Routing cho Areas
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area=Admin}/{controller=QuanLyTaiKhoan}/{action=Index}/{id?}");
+    pattern: "{area=Teacher}/{controller=Home}/{action=Index}/{id?}");
 
 // Routing mặc định
 app.MapControllerRoute(
